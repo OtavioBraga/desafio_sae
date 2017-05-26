6e3c3755e3b87290a5aefa75738697c6df929261
Otavio Braga <otavio.braga@dexatec.com.br>
2017-05-26 13:08:08 -0400

    Adicionados os models para Produto e Compra; Requirements atualizado

--- estoque/core/admin.py
+++ estoque/core/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Product, Purchase
 
-# Register your models here.
+
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('product', 'value', 'quantity', 'unit_value')
+
+    def unit_value(self, Purchase):
+        unit_price = Purchase.value / Purchase.quantity
+        return round(unit_price, 2)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Purchase, PurchaseAdmin)
--- estoque/core/models.py
+++ estoque/core/models.py
@@ -1,3 +1,57 @@
+from datetime import datetime
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(
+        'Nome do produto',
+        max_length=100,
+        blank=False,
+        null=False
+    )
+
+    average_coast = models.DecimalField(
+        'Preço médio',
+        max_digits=8,
+        decimal_places=2,
+        blank=True,
+        null=True
+    )
+
+    quantity = models.IntegerField('Quantidade', default=1)
+
+    created_date = models.DateTimeField(auto_now_add=True)
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Produto'
+        verbose_name_plural = 'Produtos'
+
+
+class Purchase(models.Model):
+    product = models.ForeignKey(Product)
+
+    value = models.DecimalField(
+        "Valor da total da compra", 
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    quantity = models.IntegerField('Quantidade', default=1)
+
+    purchase_date = models.DateTimeField(
+        'Data da compra',
+        default=datetime.now()
+    )
+
+    modified_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.product.name
+
+    class Meta:
+        verbose_name = 'Compra'
+        verbose_name_plural = 'Compras'
